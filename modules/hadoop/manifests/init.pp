70b3a04e705b5dd0b3d8de378f334764d79d7c95
sagarlekar <sagar.arlekar@gmail.com>
2013-08-18 23:09:32 +0530

    Using  hadoop-1.2.1 instead of  hadoop-1.1.1 Hadoop installable to be stored in module instead for downloading from net

--- modules/hadoop/manifests/init.pp
+++ modules/hadoop/manifests/init.pp
@@ -1,21 +1,30 @@
 class hadoop {
   $hadoop_home = "/opt/hadoop"
 
+/*
   exec { "download_hadoop":
-    command => "wget -O /tmp/hadoop.tar.gz http://apache.mirrors.timporter.net/hadoop/common/hadoop-1.1.1/hadoop-1.1.1.tar.gz",
+    command => "wget -O /tmp/hadoop.tar.gz http://apache.petsads.us/hadoop/common/hadoop-1.2.1/hadoop-1.2.1.tar.gz",
     path => $path,
-    unless => "ls /opt | grep hadoop-1.1.1",
+    unless => "ls /opt | grep hadoop-1.2.1",
     require => Package["openjdk-6-jdk"]
   }
 
   exec { "unpack_hadoop" :
     command => "tar -zxf /tmp/hadoop.tar.gz -C /opt",
     path => $path,
-    creates => "${hadoop_home}-1.1.1",
+    creates => "${hadoop_home}-1.2.1",
     require => Exec["download_hadoop"]
   }
+*/
 
-  file { "${hadoop_home}-1.1.1/conf/slaves":
+  exec { "unpack_hadoop" :
+    command => "tar -zxf /tmp/hadoop.tar.gz -C /opt",
+    path => $path,
+    creates => "${hadoop_home}-1.2.1",
+    require => File["/tmp/hadoop.tar.gz"]
+  }
+
+  file { "${hadoop_home}-1.2.1/conf/slaves":
     source => "puppet:///modules/hadoop/slaves",
     mode => 644,
     owner => root,
@@ -23,7 +32,7 @@ class hadoop {
     require => Exec["unpack_hadoop"]
   }
    
-  file { "${hadoop_home}-1.1.1/conf/masters":
+  file { "${hadoop_home}-1.2.1/conf/masters":
     source => "puppet:///modules/hadoop/masters",
     mode => 644,
     owner => root,
@@ -31,7 +40,7 @@ class hadoop {
     require => Exec["unpack_hadoop"]
   }
 
-  file { "${hadoop_home}-1.1.1/conf/core-site.xml":
+  file { "${hadoop_home}-1.2.1/conf/core-site.xml":
     source => "puppet:///modules/hadoop/core-site.xml",
     mode => 644,
     owner => root,
@@ -40,7 +49,7 @@ class hadoop {
   }
    
   file {
-    "${hadoop_home}-1.1.1/conf/mapred-site.xml":
+    "${hadoop_home}-1.2.1/conf/mapred-site.xml":
     source => "puppet:///modules/hadoop/mapred-site.xml",
     mode => 644,
     owner => root,
@@ -48,7 +57,7 @@ class hadoop {
     require => Exec["unpack_hadoop"]
   }
    
-  file { "${hadoop_home}-1.1.1/conf/hdfs-site.xml":
+  file { "${hadoop_home}-1.2.1/conf/hdfs-site.xml":
     source => "puppet:///modules/hadoop/hdfs-site.xml",
     mode => 644,
     owner => root,
@@ -56,7 +65,7 @@ class hadoop {
     require => Exec["unpack_hadoop"]
   }
    
-  file { "${hadoop_home}-1.1.1/conf/hadoop-env.sh":
+  file { "${hadoop_home}-1.2.1/conf/hadoop-env.sh":
     source => "puppet:///modules/hadoop/hadoop-env.sh",
     mode => 644,
     owner => root,
@@ -64,7 +73,7 @@ class hadoop {
     require => Exec["unpack_hadoop"]
   }
 
-  file { "${hadoop_home}-1.1.1/name":
+  file { "${hadoop_home}-1.2.1/name":
     ensure => directory,
     mode => 644,
     owner => root,
@@ -72,7 +81,7 @@ class hadoop {
     require => Exec["unpack_hadoop"]
   }
 
-  file { "${hadoop_home}-1.1.1/data":
+  file { "${hadoop_home}-1.2.1/data":
     ensure => directory,
     mode => 644,
     owner => root,
